107ca97c2c125cbd1b2f483459afba71f5e0a2f3
zhangaaron <aaron.zhang@berkeley.edu>
2016-03-10 21:02:27 -0800

    commited some code that fabric plugin told me to, idk even know man

--- App/Represent/mobile/src/main/java/com/cs160/joleary/catnip/MainActivity.java
+++ App/Represent/mobile/src/main/java/com/cs160/joleary/catnip/MainActivity.java
@@ -13,11 +13,18 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+import com.twitter.sdk.android.core.*;
+
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.location.LocationServices;
 import com.twitter.sdk.android.Twitter;
 import com.twitter.sdk.android.core.TwitterAuthConfig;
+import com.twitter.sdk.android.core.TwitterException;
+import com.twitter.sdk.android.core.TwitterSession;
+import com.twitter.sdk.android.core.identity.TwitterLoginButton;
+
 import io.fabric.sdk.android.Fabric;
 import java.util.ArrayList;
 import java.util.List;
@@ -28,6 +35,7 @@ public class MainActivity extends Activity{
     // Note: Your consumer key and secret should be obfuscated in your source code before shipping.
     private static final String TWITTER_KEY = "IWa1hAGugIHn81U4TwXzJIoc0";
     private static final String TWITTER_SECRET = "9jdUHpRAMAUaYEPJU85Xf5iqUVPQZxDGkc6DVH6TEEZHGvWSqB";
+    private TwitterLoginButton loginButton;
 
 
     private RecyclerView mRecyclerMain;
@@ -60,6 +68,8 @@ public class MainActivity extends Activity{
         super.onCreate(savedInstanceState);
         TwitterAuthConfig authConfig = new TwitterAuthConfig(TWITTER_KEY, TWITTER_SECRET);
         Fabric.with(this, new Twitter(authConfig));
+
+
         setContentView(R.layout.activity_main);
         Bundle b = getIntent().getExtras();
         if (b != null) {
@@ -108,6 +118,25 @@ public class MainActivity extends Activity{
 //            _list.add(dummy_rep);
 //        }
 
+
+        loginButton = (TwitterLoginButton) findViewById(R.id.twitter_login_button);
+        loginButton.setCallback(new Callback<TwitterSession>() {
+            @Override
+            public void success(Result<TwitterSession> result) {
+                // The TwitterSession is also available through:
+                // Twitter.getInstance().core.getSessionManager().getActiveSession()
+                TwitterSession session = result.data;
+                // TODO: Remove toast and use the TwitterSession's userID
+                // with your app's user model
+                String msg = "@" + session.getUserName() + " logged in! (#" + session.getUserId() + ")";
+                Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_LONG).show();
+            }
+            @Override
+            public void failure(TwitterException exception) {
+                Log.d("TwitterKit", "Login with Twitter failure", exception);
+            }
+        });
+
         //set adapter
         mAdapter = new RepDataAdapter(_list, getApplicationContext());
         mRecyclerMain.setAdapter(mAdapter);
@@ -140,5 +169,14 @@ public class MainActivity extends Activity{
 
         return super.onOptionsItemSelected(item);
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        // Make sure that the loginButton hears the result from any
+        // Activity that it triggered.
+        loginButton.onActivityResult(requestCode, resultCode, data);
+    }
+
 
 }
